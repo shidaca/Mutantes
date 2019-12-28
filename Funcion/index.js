0c020e5704f4be13f269eb6016db545d5c34f1d1
shidaca <shidacalderon@gmail.com>
2019-12-28 20:17:44 -0300

    actualizacion repositorio

--- Funcion/index.js
+++ Funcion/index.js
@@ -1,32 +1,4 @@
-const pg = require('pg');
- 
-
-const cs = 'postgres://postgres:s$cret@localhost:5432/Mutantes';
-
-const client = new pg.Client(
-    {
-        host:  'localhost',
-        port: 5432,
-        database: 'Mutantes',
-        user: 'postgres',
-        password: '121314',
-      }
-    
-    );
-    client
-    .connect()
-    .then(() => console.log('connected'))
-    .catch(err => console.error('connection error', err.stack));
-
-
-
-
-
-
 const isMutant = async (genes = []) => {
-    const query =  'INSERT INTO estadistica(ismutant,adn) VALUES($1, $2)';
-
-
     let diagonal = [undefined, 0];
     let vertical = [undefined, 0];
     let horizontal = [undefined, 0];
@@ -47,15 +19,7 @@ const isMutant = async (genes = []) => {
             }
             if (horizontal[1] == 4) {
                 console.log("Fila: " + geneIndex);
-                
-                await client.query(query, [true,genes] ,(err, res) => {
-    if (err) {
-      console.log(err.stack)
-    } else {
-      console.log(res.rows[0])
-    }
-  })
-  return true;
+                return true;
             }
 
             //vertical
@@ -70,14 +34,7 @@ const isMutant = async (genes = []) => {
             }
 
             if (vertical[1] == 4) {
-                alert("Columna: " + geneIndex);
-                await  client.query(query, [true,genes] ,(err, res) => {
-                    if (err) {
-                      console.log(err.stack)
-                    } else {
-                      console.log(res.rows[0])
-                    }
-                  })
+                console.log("Columna: " + geneIndex);
                   return true;
             }
         }
@@ -96,26 +53,13 @@ const isMutant = async (genes = []) => {
 
         if (diagonal[1] == 4) {
             console.log("Diagonal: " + geneIndex);
-            await   client.query(query, [true,genes] ,(err, res) => {
-                if (err) {
-                  console.log(err.stack)
-                } else {
-                  console.log(res.rows[0])
-                }
-              })
               return true;
         }
 
         horizontal = [undefined, 0];
         vertical = [undefined, 0];
     }
-  await  client.query(query, [true, JSON.stringify(genes)] ,(err, res) => {
-        if (err) {
-          console.log(err.stack)
-        } else {
-          console.log(res.rows[0])
-        }
-      })
+
       return false;
 };
 
--- routes/mutantes.js
+++ routes/mutantes.js
@@ -1,16 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const isMutant = require('../Funcion');
+const pg = require('pg');
+const cs = 'postgres://postgres:s$cret@localhost:5432/Mutantes';
 
+const client = new pg.Client(
+    {
+        host:  'localhost',
+        port: 5432,
+        database: 'Mutantes',
+        user: 'postgres',
+        password: '121314',
+      }
+    
+    );
+    client.connect()
+    const query =  'INSERT INTO estadistica(ismutante,adn) VALUES($1, $2)';
 /* GET home page. */
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
+  
   //res.render('index', { title: 'Express' });
   let body= req.body;
   console.log(body);
   let dna =body && body.dna;//algo que se llame data y meterlo en var result 
   console.log(dna);
-let result = isMutant.isMutant(dna)
-  res.send({ 'isMutant': result })
+  let result = await isMutant.isMutant(dna);
+  if(result){
+    await  client.query(query, [result, dna] ,(err, res) => {
+      if (err) {
+        console.log(err.stack)
+      } else {
+        console.log(res.rows[0])
+      }
+    })
+    res.status(200).send({ 'isMutant': result })//mensajes ok
+  }
+  res.status(403).send({ 'isMutant': result })// mensajer error 
 });
 
 module.exports = router;
